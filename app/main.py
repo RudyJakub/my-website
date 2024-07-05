df4f68f4e314f95a801f89bd4d0d43f5ce03ee24
JakubRudy <yobomedas@Jaskier>
2024-07-05 02:36:44 +0200

    nginx

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from database import db
+from models import *
 
 app = Flask(__name__)
 
@@ -16,5 +17,7 @@ def about():
 def electric_field():
     return render_template('electric_field.html')
 
+"""
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
+"""
